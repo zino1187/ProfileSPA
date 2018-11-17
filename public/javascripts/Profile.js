e25f8eaed4ad74ae82da81c802e9f59712ea7ba4
koitt-04-P-PC\koitt-04-P <zino1187@gmail.com>
2018-11-17 15:57:10 +0900

    no message

--- public/javascripts/Profile.js
+++ public/javascripts/Profile.js
@@ -17,7 +17,12 @@ class Profile{
 
         //코드로 ul 세트를 정의 
         this.ul=document.createElement("ul");
-        
+        //순수 자바스크립트에서 이벤트 핸들러를 동적
+        //으로 구현하는 방법
+        this.ul.addEventListener("click", ()=>{
+            getDetail(this.arr);
+        });
+
         for(var i=0;i<this.arr.length;i++){
             var li=document.createElement("li");
             li.innerText=this.arr[i];
